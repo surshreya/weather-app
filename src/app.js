6efdfde9734863b17853d17797161ddac8be812c
surshreya <surshreya.0727@gmail.com>
2023-01-09 17:05:23 +0530

    setting up client side

--- src/app.js
+++ src/app.js
@@ -1,15 +1,32 @@
+const path = require("path");
 /**
  * Importing external dependencies
  */
 const chalk = require("chalk");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
-
 const port = process.env.PORT;
 
+//Define paths for express configuration
+const publicDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+//Set up view engine and views path
+app.set("view-engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Serves the static assets
+app.use(express.static(publicDirPath));
+
 app.get("", (req, res) => {
-  res.send("Hello world");
+  res.render("index.hbs", {
+    title: "Weather App",
+    name: "Shreya Sur",
+  });
 });
 
 app.get("/about", (req, res) => {
